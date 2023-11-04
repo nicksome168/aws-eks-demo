f46b08759af0604ca04744f828fe554684d12c42
Nick Chao <yc6371@nyu.edu>
2023-11-04 15:49:11 -0400

    debug terraform aws cred path

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["/.aws/credentials"]
+  shared_credentials_files = ["/home/vscode/.aws/credentials"]
   profile                  = "terraform"
 }
